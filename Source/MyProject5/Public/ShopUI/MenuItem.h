173978465036ca99c6cf9ca0030772440d85f0f8
Sapodilla <jakebaxter8@hotmail.co.uk>
2023-08-31 10:30:56 +0100

    Recreated ShopWidget

---
+++ Source/MyProject5/Public/ShopUI/MenuItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT5_API UMenuItem : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
